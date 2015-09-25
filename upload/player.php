86bdd1655d79565b7786a21b92f3443fe95890b3
lioshi <lionel.fenneteau@gmail.com>
2015-09-25 02:07:30 +0200

    Update player.php

--- upload/player.php
+++ upload/player.php
@@ -23,7 +23,7 @@ $return = array();
 
 $return['urlVideos']= $vdo;
 $return['urlDefaulThumb']= get_thumb($vdetails);
-$return['urlThumbs']= get_thumb($vdetails, 'default', true);
+$return['urlThumbs']= get_thumb($vdetails, 'default', true); 
 $return['details']=$vdetails;
 
 
@@ -39,7 +39,34 @@ video {
 </style>';
 echo '</head>';
 echo '<body>';
-echo '<video width="100%" controls src="'.$vdo[0].'" autoplay="true" poster="'.get_thumb($vdetails).'">'.$vdetails['title'].'</video>';
+
+$command = 'mediainfo -f --Output=JSON '. __DIR__.'/files/videos/'.$vdetails['file_directory'].'/'.basename($vdo['0']);
+exec($command,$output);
+
+//echo '<pre>';
+//var_dump($output);
+//echo '</pre>';
+
+$infoReturn = array();
+foreach($output as $line){
+	$key=trim(substr($line,0,strpos($line,':')));
+	$value=trim(substr($line,strpos($line,':')+1));
+//echo "B>".$key."<br/>";
+        
+	$key = keyRename($key,$infoReturn);
+//echo "A>".$key."<br/>";
+	$infoReturn[$key]=$value;
+
+}
+//echo '<pre>';
+//var_dump($infoReturn);
+//echo '</pre>';
+
+$ratio = $infoReturn['Display aspect ratio'];
+$w = $infoReturn['Width'];
+$h = $infoReturn['Height'];
+
+echo '<video data-width="'.$w.'" data-height="'.$h.'"  data-ratio="'.$ratio.'" width="100%" controls src="'.$vdo[0].'" autoplay="true" poster="'.get_thumb($vdetails).'">'.$vdetails['title'].'</video>';
 echo '</body>';
 echo '</html>';
 
@@ -47,3 +74,15 @@ echo '</html>';
 //echo "<pre>";
 //echo json_encode($return);
 //echo "</pre>";
+
+
+function keyRename($key,$array){
+        if (array_key_exists($key, $array)) {
+               	//	return $key.'_1';
+		return keyRename($key.'-',$array);
+        } else {
+		return $key;
+	}
+}
+
+
